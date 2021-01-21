9858594d4ffd51eb178ca531c42a37bf561107f5
Jamie Pham <jpham1109@gmail.com>
2021-01-20 22:20:45 -0500

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,53 +1,34 @@
-# require modules here
-require "yaml"
-require "pry"
+require 'yaml'
+require 'pry'
 file = YAML.load_file('./lib/emoticons.yml')
 puts file.inspect
 
 def load_library(file)
-  
-  file = YAML.load_file(---'./lib/emoticons.yml')
+  file = YAML.load_file('./lib/emoticons.yml')
   emoticons = file.each_with_object({}) do |(k, v), emoticons|
-    lang = [:english, :japanese]
-    emoticons[k] = Hash[lang[0], v[0], lang[1], v[1]]
-  end
+    emoticons[k] = Hash[:english, v[0], :japanese, v[1]]
+  end 
   emoticons
-  #binding.pry
 end
-#binding.pry 
-
-
-def get_japanese_emoticon(file, w_emoticon)
-  emoticons = load_library(file)
-  j_emoticon = "Sorry, that emoticon was not found"
-  
-  #binding.pry
-  emoticons.each do |k,v|
-    #w_emoticon = emoticons[k][:english]
-    #binding.pry
-    if emoticons[k].value?(w_emoticon)
-      j_emoticon = emoticons[k][:japanese]
-      #binding.pry
-    end
-    #binding.pry
-  end
-  j_emoticon
-end
-
-
 
 def get_english_meaning(file, j_emoticon)
   emoticons = load_library(file)
-  english = "Sorry, that emoticon was not found"
-  #binding.pry
-  emoticons.each do |k, v|
-    #binding.pry
-    if emoticons[k].value?(j_emoticon)
-      english = k
-    #binding.pry
+  result = "Sorry, that emoticon was not found"
+  emoticons.each do |meaning, emotes|
+    if j_emoticon == emotes[:japanese]
+      result = meaning 
     end
-    #binding.pry
   end
-  #binding.pry
-  english
+  result
+end
+
+def get_japanese_emoticon(file, w_emoticon)
+  emoticons = load_library(file)
+  result = "Sorry, that emoticon was not found"
+  emoticons.each do |meaning, emotes|
+    if w_emoticon == emotes[:english]
+      result = emotes[:japanese]
+    end 
+  end 
+  result
 end
